8f8c124df3c1d285d7eb1a857efae37cf1857ec9
Ivan Torres <pinguxx@gmail.com>
2015-04-04 23:42:09 -0500

    update calendar version

--- DatePicker.js
+++ DatePicker.js
@@ -265,7 +265,7 @@ var DatePicker = function (properties) {
                 }
             }),
             m('.sm-calendar', {
-                style: 'display:' + datePicker.display() + ';position:absolute;left:4px;top:35px;background-color:#fff;border:1px solid gray;z-index:100;border-radius:0 0 5px 5px;box-shadow:2px 2px 3px gray;min-width:300px;'
+                style: 'display:' + datePicker.display() + ';position:absolute;left:4px;top:35px;background-color:#fff;border:1px solid gray;z-index:100;border-radius:0 0 5px 5px;box-shadow:2px 2px 3px gray;min-width:321px;'
             }, datePicker.calendar.view()),
             m('button.ui.icon.button', {
                 onclick: function (e) {
